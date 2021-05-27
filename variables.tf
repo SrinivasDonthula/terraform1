5339b9c251d70116deb0e71e13758209b9b058a3
root <root@ip-172-31-5-25.ap-southeast-1.compute.internal>
2021-05-27 06:59:43 +0000

    third commit

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+   type = string
+   default = "ap-southeast-1"
+}
+
+variable "access_key" {
+   type = string
+   default = "AKIA5EI7BTUNHAGXE3HF"
+}
+
+variable "secret_key" {
+   type = string
+   default = "Exe9if3z31qbOTaZ+3rvg66t6gGrLwypBNbh2AAZ"
+}
